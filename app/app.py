e8822d8dd07134408d40191300d4b0eb44658f02
root <root@DESKTOP-ICEHHUR.localdomain>
2020-10-28 23:49:40 +0800

    add dns page

--- app/app.py
+++ app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/dnsconfig")
+def dnsconfig():
+    return render_template("dnsconfig.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
