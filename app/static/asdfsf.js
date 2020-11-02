6662a4736747003acdba043df359d8ee796f4555
WilliamRyank <kwilliamryan@yahoo.com>
2020-11-02 17:35:35 +0800

    finish login page

---
+++ app/static/asdfsf.js
@@ -0,0 +1,113 @@
+
+function eujfsdf909sdfsdiid() {
+    console.log('Hi');
+}
+
+function dns() {
+    console.log('But, you still');
+    con();
+}
+
+function sdgf9dffjgjdsafds() {
+    console.log('Hi');
+}
+
+function iasdfdaijfisdjiid() {
+    console.log('Hi');
+}
+
+function pathIs() {
+    console.log('Nice try');
+    slash();
+}
+
+function dfafnidsfisainfki() {
+    console.log('Hi');
+}
+
+function vevve0fqwf2jef0wje() {
+    console.log('Hi');
+}
+
+function eujfsd3909sdfsdiid() {
+    console.log('Hi');
+}
+
+function zdsjfkasdfkjsdfasn() {
+    console.log('Hi');
+}
+
+function slash() {
+    console.log('Bypassing the login.');
+    dns()
+}
+
+function ijifjvidjijiiddddv() {
+    console.log('Hi');
+}
+
+function iobaiobnoab0iadsvi() {
+    console.log('Hi');
+}
+
+function con() {
+    console.log('Cant access the');
+    fig();
+}
+
+function vevve0fqwfwje60wje() {
+    console.log('Hi');
+}
+
+function eujfsdf909s7fsdiid() {
+    console.log('Hi');
+}
+
+function adsjfkasd8kjsdfasn() {
+    console.log('Hi');
+}
+
+function sdgf10sdfasfdsafds() {
+    console.log('Hi');
+}
+
+function byp4ss1ngL0g1n() {
+    window.location.href = '/dnsconfig';
+}
+
+function iobai13noabiiadsvi() {
+    console.log('Hi');
+}
+
+function vevve0f23fwjef0wje() {
+    console.log('Hi');
+}
+
+function eujfsdf913sdfsdiid() {
+    console.log('Hi');
+}
+
+function adsjfkasd00jsdfasn() {
+    console.log('Hi');
+}
+
+function sdgfgas11asfdsafds() {
+    console.log('Hi');
+}
+
+function ijifjvidji32iddddv() {
+    console.log('Hi');
+}
+
+function iobaiobnopapbiiadsvi() {
+    console.log('Hi');
+}
+
+function fig() {
+    console.log('page :)');
+}
+
+function eujfsdf9gkfjffsdiid() {
+    console.log('Hi');
+}
+
