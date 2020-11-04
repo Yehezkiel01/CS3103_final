978059b62301228d46707f4c140bb063bd255ef0
Yehezkiel Raymundo Theodoroes <yehezkieltheodoroes@yahoo.co.id>
2020-11-04 17:55:19 +0800

    Implement the endpoints for querying sql

--- app/app.py
+++ app/app.py
@@ -1,8 +1,34 @@
 from flask import Flask, render_template
 from flask import jsonify
 from flask import request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# MY SQL Configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'dns'
+
+mysql = MySQL(app)
+
+# Open connection with sql to query the domain
+def query_domain(name):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM records WHERE domain='" + name + "'")
+    results = cur.fetchall()
+    cur.close()
+
+    # Change the format
+    formatted_results = []
+    for record in results:
+        formatted_results.append({
+            "domain": record[0],
+            "ip": record[1]
+        })
+
+    return formatted_results
 
 @app.route("/")
 def index():
@@ -15,9 +41,10 @@ def dnsconfig():
 # /dnsconfig/search?query=something
 @app.route("/dnsconfig/search")
 def search():
-    query = request.args.get('query')
+    query = request.args.get('query') or ""
     print("This is your query: " +  query)
-    return jsonify(query)
+    results = query_domain(query)
+    return jsonify(results)
 
 if __name__ == "__main__":
     app.run(debug=True)
