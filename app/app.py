c2f292b4475e0b1f959362ccc0df1bfe41701333
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2020-11-04 13:36:31 +0800

    Implmenet basic query endpont

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,6 +11,13 @@ def index():
 @app.route("/dnsconfig")
 def dnsconfig():
     return render_template("dnsconfig.html")
+
+# /dnsconfig/search?query=something
+@app.route("/dnsconfig/search")
+def search():
+    query = request.args.get('query')
+    print("This is your query: " +  query)
+    return jsonify(query)
 
 if __name__ == "__main__":
     app.run(debug=True)
