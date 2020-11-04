600279a89a817680dd70fd09338f30d08f4cba10
Yehezkiel Raymundo Theodoroes <yehezkieltheodoroes@yahoo.co.id>
2020-11-04 21:38:50 +0800

    Add sqlite database

---
+++ app/database/dns.sql
@@ -0,0 +1,8 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE records (
+domain varchar(255) NOT NULL PRIMARY KEY,
+ip varchar(255) NOT NULL
+);
+INSERT INTO "records" VALUES('www.bank.com','127.0.0.1');
+COMMIT;
